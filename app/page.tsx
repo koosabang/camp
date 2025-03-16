77f60c733e33c6d0a9180fecccd021bf4fcd77d1
koosabang <koosabang@gmail.com>
2025-03-16 17:57:30 +0700

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+//rafce
+import {Button} from "@/components/ui/button"
+const HomePage = () => {
+  return (
+    <div>HomePage
+      <Button variant="outline">Button</Button>
+
+    </div>
+  )
+}
+export default HomePage
---
+++ components/Navbar/DropdownListMenu.tsx
@@ -0,0 +1,39 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { AlignLeft } from "lucide-react";
+import { Button } from "../ui/button";
+import UserIcon from "./UserIcon";
+import Link from "next/link";
+import { links } from "@/utils/links";
+
+
+const DropDownListMenu = () => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant={"outline"}>
+          <AlignLeft />
+          <UserIcon />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        {
+          links.map((item, index)=>{
+            return <DropdownMenuItem key={index}>
+              <Link href={item.href}>{item.label}</Link>
+            </DropdownMenuItem>
+          })
+        }    
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+export default DropDownListMenu;
